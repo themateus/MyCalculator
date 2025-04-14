a2ed5ba08756293ef41b63b2a9a6bbd5e798c608
Mateus Lucena <mateuslucena2014@gmail.com>
2025-04-14 20:50:15 -0300

    add

---
+++ MyCalculator/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Text.RegularExpressions;
+using MyCalculatorLibrary;
+
+namespace MyCalculatorProgram;
+
+static class Program
+{
+    static void Main()
+    {
+        bool endApp = false;
+        // Display title as the C# console calculator app.
+        Console.WriteLine("Console Calculator in C#\r");
+        Console.WriteLine("------------------------\r");
+
+        while (!endApp)
+        {
+            // Declare variables and set to empty
+            // Use Nullable types (with ?) to match type of System.Console.ReadLine
+            string? numInput1;
+            string? numInput2;
+            double result;
+            
+            // Ask the user to type the first number.
+            Console.WriteLine("Type a number, and then press Enter: ");
+            numInput1 = Console.ReadLine();
+
+            double cleanNum1;
+            while (!double.TryParse(numInput1, out cleanNum1))
+            {
+                Console.WriteLine("This is not valid input. Please enter a numeric value: ");
+                numInput1 = Console.ReadLine();
+            }
+            
+            // Ask the user to type the second number.
+            Console.WriteLine("Type another number, and then press Enter: ");
+            numInput2 = Console.ReadLine();
+
+            double cleanNum2;
+            while (!double.TryParse(numInput2, out cleanNum2))
+            {
+                Console.WriteLine("This is not valid input. Please enter a numeric value: ");
+                numInput2 = Console.ReadLine();
+            }
+            
+            // Ask the user to choose an operator.
+            Console.WriteLine("Choose an operator from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.WriteLine("Your option? ");
+
+            string? op = Console.ReadLine();
+            
+            // Validate input is not null, and matches the pattern
+            if (op == null || !Regex.IsMatch(op, "[a|s|m|d]"))
+            {
+                Console.WriteLine("Error: Unrecognized input.");
+            }
+            else
+            {
+                try
+                {
+                    result = MyCalculator.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("This operation will result in a mathematical error.\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your result: {0:0.##}\n", result);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                }
+            }
+            Console.WriteLine("--------------------------\n");
+            
+            // Wait for the user to respond before closing.
+            Console.WriteLine("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+            if (Console.ReadLine() == "n") endApp = true;
+
+            Console.WriteLine("\n"); // Friendly line spacing.
+        }
+    }
+}
---
+++ MyCalculatorLibrary/MyCalculatorLibrary.cs
@@ -0,0 +1,32 @@
+﻿namespace MyCalculatorLibrary;
+
+public static class MyCalculator
+{
+    public static double DoOperation(double num1, double num2, string op)
+    {
+        double result = double.NaN; //Default value is "Not-a-number" if an operation, as division, could result in an error.
+        
+        // Switch statement to do the math.
+        switch (op)
+        {
+            case "a":
+                result = num1 + num2;
+                break;
+            case "s":
+                result = num1 - num2;
+                break;
+            case "m":
+                result = num1 * num2;
+                break;
+            case "d":
+                // Ask the user to enter a non-zero divisor.
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                break;
+            // Return text for an incorrect option entry.
+        }
+        return result;
+    }
+}
